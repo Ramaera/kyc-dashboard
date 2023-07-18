f8d1fc28084d1eda07ad21aa59d70f829f0a2283
rajnishramaera <rajnish.it@ramaera.in>
2023-07-18 16:28:52 +0530

    delete page

--- out/_next/static/chunks/pages/agency/Tabs/Projects-22ebcbdeec1d2875.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1694],{24342:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/agency/Tabs/Projects",function(){return u(77471)}])}},function(n){n.O(0,[6093,9990,1162,5133,2937,7058,315,2738,8584,6154,7322,7165,7471,9774,2888,179],(function(){return _=24342,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
