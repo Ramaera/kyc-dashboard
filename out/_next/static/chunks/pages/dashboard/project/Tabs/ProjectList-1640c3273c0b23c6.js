3bdba3350e2d0dcea4f767a97fdd580c7167abbc
rajnishramaera <rajnish.it@ramaera.in>
2023-09-05 14:20:25 +0530

    variable file

---
+++ out/_next/static/chunks/pages/dashboard/project/Tabs/ProjectList-1640c3273c0b23c6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7989],{64973:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/dashboard/project/Tabs/ProjectList",function(){return u(26515)}])}},function(n){n.O(0,[9774,5370,9990,3265,6180,1880,6549,7058,315,6261,6372,481,6515,2888,179],(function(){return _=64973,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
