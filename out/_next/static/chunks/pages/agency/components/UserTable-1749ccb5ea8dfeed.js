3ab30025cbd11335db3f70510f62618800d06539
Rajnish Pratap Singh <125359327+rajnishramaera@users.noreply.github.com>
2023-10-21 11:01:08 +0530

    next version updated to 12.3.2

--- out/_next/static/chunks/pages/agency/components/UserTable-1749ccb5ea8dfeed.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6940],{90258:function(n,_,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/agency/components/UserTable",function(){return e(12475)}])}},function(n){n.O(0,[5370,9990,2052,4753,6827,6549,7058,788,135,1350,6261,4549,2859,599,2475,9774,2888,179],(function(){return _=90258,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
--- out/_next/static/chunks/pages/dashboard/project/Tabs/ProjectList-0654bb4d0a224573.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7989],{64973:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/dashboard/project/Tabs/ProjectList",function(){return u(26515)}])}},function(n){n.O(0,[9774,5370,9990,2052,4753,6827,6549,7058,788,135,6261,4549,2859,9390,6515,2888,179],(function(){return _=64973,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
