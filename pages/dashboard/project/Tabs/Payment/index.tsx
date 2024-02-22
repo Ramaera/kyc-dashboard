7a084c8194d99a27cdd4cc8f10223a26082382de
mohansharma916 <mohansharma916@gmail.com>
2024-02-22 06:41:10 +0530

    fixed

--- pages/dashboard/project/Tabs/Payment/index.tsx
+++ pages/dashboard/project/Tabs/Payment/index.tsx
@@ -766,6 +766,60 @@ const InfoTab = ({ title }) => {
                     <br />
                   </>
                 )}
+                {showBankDetails && enrollNow && (
+                  <TableContainer component={Paper} sx={{ mt: 2 }}>
+                    <Table sx={{ minWidth: 100 }} aria-label="simple table">
+                      <TableBody>
+                        {AllBankDetails[title + 'BankDetails'].map(
+                          (bankData) => {
+                            return (
+                              <TableRow
+                                key={bankData.key}
+                                sx={{
+                                  '&:last-child td, &:last-child th': {
+                                    border: 0
+                                  }
+                                }}
+                              >
+                                <TableCell component="th" scope="row">
+                                  {bankData.key}
+                                </TableCell>
+                                <TableCell align="right">
+                                  {bankData.info}
+                                </TableCell>
+                              </TableRow>
+                            );
+                          }
+                        )}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                )}
+
+                {showUPIDetails &&
+                  enrollNow &&
+                  title.toLowerCase() === 'hyderabad' && (
+                    <TableContainer component={Paper} sx={{ mt: 2 }}>
+                      <Table sx={{ minWidth: 100 }} aria-label="simple table">
+                        <TableBody>
+                          <img
+                            style={{
+                              width: '250px',
+                              borderRadius: '20px',
+                              padding: '10px'
+                            }}
+                            src="/images/hyderabad_upi.png"
+                          />
+                          <Typography variant="body1" sx={{ my: 2, pl: 2 }}>
+                            OR
+                          </Typography>
+                          <Typography variant="h4" sx={{ my: 2, pl: 2 }}>
+                            <a href="#">UPI ID : ramaeraindustries@sbi</a>
+                          </Typography>
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  )}
                 {proofImage ? (
                   <img
                     src={
@@ -803,55 +857,6 @@ const InfoTab = ({ title }) => {
                   </Button>
                 )}
               </div>
-
-              {showBankDetails && enrollNow && (
-                <TableContainer component={Paper} sx={{ mt: 2 }}>
-                  <Table sx={{ minWidth: 100 }} aria-label="simple table">
-                    <TableBody>
-                      {AllBankDetails[title + 'BankDetails'].map((bankData) => {
-                        return (
-                          <TableRow
-                            key={bankData.key}
-                            sx={{
-                              '&:last-child td, &:last-child th': { border: 0 }
-                            }}
-                          >
-                            <TableCell component="th" scope="row">
-                              {bankData.key}
-                            </TableCell>
-                            <TableCell align="right">{bankData.info}</TableCell>
-                          </TableRow>
-                        );
-                      })}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-              )}
-
-              {showUPIDetails &&
-                enrollNow &&
-                title.toLowerCase() === 'hyderabad' && (
-                  <TableContainer component={Paper} sx={{ mt: 2 }}>
-                    <Table sx={{ minWidth: 100 }} aria-label="simple table">
-                      <TableBody>
-                        <img
-                          style={{
-                            width: '250px',
-                            borderRadius: '20px',
-                            padding: '10px'
-                          }}
-                          src="/images/hyderabad_upi.png"
-                        />
-                        <Typography variant="body1" sx={{ my: 2, pl: 2 }}>
-                          OR
-                        </Typography>
-                        <Typography variant="h4" sx={{ my: 2, pl: 2 }}>
-                          <a href="#">UPI ID : ramaeraindustries@sbi</a>
-                        </Typography>
-                      </TableBody>
-                    </Table>
-                  </TableContainer>
-                )}
 
               {enrollNow && (
                 <Grid container py={2} spacing={2}>
