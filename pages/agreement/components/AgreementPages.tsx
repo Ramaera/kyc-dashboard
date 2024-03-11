9da22ecde0b6888a25f9fea761a21568c036c49f
mohansharma916 <mohansharma916@gmail.com>
2024-03-11 12:38:11 +0530

    agreement hide

--- pages/agreement/components/AgreementPages.tsx
+++ pages/agreement/components/AgreementPages.tsx
@@ -15,83 +15,83 @@
 // import { PDFViewer } from '@react-pdf/renderer';
 // import ReactDOM from 'react-dom';
 
-// const AgreementPages = () => {
-//   const generatePdf = async () => {
-//     // const input = document.getElementById('pdf-content');
-//     // const inputHeight = input.offsetHeight;
-//     // const inputWidth = input.offsetWidth;
-//     // const pdf = new jsPDF('p', 'mm');
-//     // const margin = 30; // Margin from top and bottom
-//     // const pageSize = pdf.internal.pageSize;
-//     // const pageWidth = pageSize.width;
-//     // const pageHeight = 280;
-//     // const scale = 2;
-//     // html2canvas(input, {
-//     //   scale: scale,
-//     //   scrollY: 0,
-//     //   windowWidth: document.body.scrollWidth + 5,
-//     //   windowHeight: document.body.scrollHeight,
-//     //   useCORS: true, // Enable CORS for external images
-//     //   allowTaint: true, // Allow rendering of images from other domains
-//     //   logging: true, // Enable logging for debugging
-//     //   width: inputWidth,
-//     //   height: inputHeight
-//     // }).then((canvas) => {
-//     //   const imgData = canvas.toDataURL('image/png');
-//     //   const imgWidth = 210;
-//     //   const pageHeight = 295;
-//     //   const imgHeight = (canvas.height * imgWidth) / canvas.width;
-//     //   var heightLeft = imgHeight;
-//     //   let position = 0;
-//     //   pdf.addImage(imgData, 'JPEG', 0, position + 20, imgWidth, imgHeight);
-//     //   heightLeft -= pageHeight;
-//     //   while (heightLeft >= 0) {
-//     //     position = heightLeft - imgHeight;
-//     //     pdf.addPage();
-//     //     pdf.addImage(imgData, 'JPEG', 0, position, imgWidth, imgHeight);
-//     //     heightLeft -= pageHeight;
-//     //   }
-//     //   pdf.save('download1.pdf');
-//     // });
+const AgreementPages = () => {
+  //   const generatePdf = async () => {
+  //     // const input = document.getElementById('pdf-content');
+  //     // const inputHeight = input.offsetHeight;
+  //     // const inputWidth = input.offsetWidth;
+  //     // const pdf = new jsPDF('p', 'mm');
+  //     // const margin = 30; // Margin from top and bottom
+  //     // const pageSize = pdf.internal.pageSize;
+  //     // const pageWidth = pageSize.width;
+  //     // const pageHeight = 280;
+  //     // const scale = 2;
+  //     // html2canvas(input, {
+  //     //   scale: scale,
+  //     //   scrollY: 0,
+  //     //   windowWidth: document.body.scrollWidth + 5,
+  //     //   windowHeight: document.body.scrollHeight,
+  //     //   useCORS: true, // Enable CORS for external images
+  //     //   allowTaint: true, // Allow rendering of images from other domains
+  //     //   logging: true, // Enable logging for debugging
+  //     //   width: inputWidth,
+  //     //   height: inputHeight
+  //     // }).then((canvas) => {
+  //     //   const imgData = canvas.toDataURL('image/png');
+  //     //   const imgWidth = 210;
+  //     //   const pageHeight = 295;
+  //     //   const imgHeight = (canvas.height * imgWidth) / canvas.width;
+  //     //   var heightLeft = imgHeight;
+  //     //   let position = 0;
+  //     //   pdf.addImage(imgData, 'JPEG', 0, position + 20, imgWidth, imgHeight);
+  //     //   heightLeft -= pageHeight;
+  //     //   while (heightLeft >= 0) {
+  //     //     position = heightLeft - imgHeight;
+  //     //     pdf.addPage();
+  //     //     pdf.addImage(imgData, 'JPEG', 0, position, imgWidth, imgHeight);
+  //     //     heightLeft -= pageHeight;
+  //     //   }
+  //     //   pdf.save('download1.pdf');
+  //     // });
 
-//     const pdfBlob = await generatePDFBlob();
-//     saveAs(pdfBlob, 'document.pdf');
-//   };
+  //     const pdfBlob = await generatePDFBlob();
+  //     saveAs(pdfBlob, 'document.pdf');
+  //   };
 
-//   const generatePDFBlob = async () => {
-//     // Create a hidden div to render the PDF
-//     const div = document.createElement('div');
-//     document.body.appendChild(div);
+  //   const generatePDFBlob = async () => {
+  //     // Create a hidden div to render the PDF
+  //     const div = document.createElement('div');
+  //     document.body.appendChild(div);
 
-//     // Render PDF component within the div
-//     ReactDOM.render(
-//       <PDFViewer width="800" height="600">
-//         <AgreementPages />
-//       </PDFViewer>,
-//       div
-//     );
+  //     // Render PDF component within the div
+  //     ReactDOM.render(
+  //       <PDFViewer width="800" height="600">
+  //         <AgreementPages />
+  //       </PDFViewer>,
+  //       div
+  //     );
 
-//     // Wait for rendering
-//     await new Promise((resolve) => setTimeout(resolve, 1000));
+  //     // Wait for rendering
+  //     await new Promise((resolve) => setTimeout(resolve, 1000));
 
-//     // Convert the rendered PDF to blob
-//     const pdfBlob = await new Promise((resolve) => {
-//       const canvas = div.querySelector('canvas');
-//       canvas.toBlob((blob) => {
-//         resolve(blob);
-//       });
-//     });
+  //     // Convert the rendered PDF to blob
+  //     const pdfBlob = await new Promise((resolve) => {
+  //       const canvas = div.querySelector('canvas');
+  //       canvas.toBlob((blob) => {
+  //         resolve(blob);
+  //       });
+  //     });
 
-//     // Clean up
-//     document.body.removeChild(div);
+  //     // Clean up
+  //     document.body.removeChild(div);
 
-//     return pdfBlob;
-//   };
+  //     return pdfBlob;
+  //   };
 
-//   const contentRef = useRef(null);
-//   return (
-//     <>
-//       <PDFViewer>
+  //   const contentRef = useRef(null);
+  return (
+    <>
+      {/* //       <PDFViewer>
 //         {/* <div
 //           ref={contentRef}
 //           id="pdf-content"
@@ -101,22 +101,19 @@
 //             backgroundColor: 'white'
 //           }}
 //         > */}
-//         <Document
-//           style={{
-//             width: '80%',
-//             padding: 40,
-//             backgroundColor: 'white'
-//           }}
-//         >
-//           <MainPage />
-//           {/* <OtherPage /> */}
-//         </Document>
-//         {/* </div> */}
-//       </PDFViewer>
+      {/* //         <Document */}
+      {/* //           style={{ */}
+      // width: '80%', // padding: 40, // backgroundColor: 'white'
+      {/* //           }} */}
+      {/* //         > */}
+      {/* //           <MainPage /> */}
+      // {/* <OtherPage /> */}
+      {/* //         </Document> */}
+      // {/* </div> */}
+      {/* //       </PDFViewer> */} */
+      {/* //       <button onClick={generatePdf}>Generate PDF</button> */}
+    </>
+  );
+};
 
-//       <button onClick={generatePdf}>Generate PDF</button>
-//     </>
-//   );
-// };
-
-// export default AgreementPages;
+export default AgreementPages;
