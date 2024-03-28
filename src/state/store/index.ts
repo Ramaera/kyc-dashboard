e18d2d20f2986f68ba9bcbbddd20a007b76ec099
Owais <owais.it@ramaera.in>
2024-03-28 12:12:15 +0530

    reload data lost issue fix

--- src/state/store/index.ts
+++ src/state/store/index.ts
@@ -1,25 +1,36 @@
 import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import allUsersSlice from '../slice/allUsersSlice';
 import foundUserSlice from '../slice/foundUserSlice';
 import userSlice from '../slice/userSlice';
-
 import allShareHolderSlice from '../slice/allShareHolderSlice';
-
 import filtersSlice from '../slice/filtersSlice';
 import walletBalanceSlice from '../slice/walletBalanceSlice';
 
-export const store = configureStore({
-  reducer: {
-    user: userSlice,
-    allUsers: allUsersSlice,
-    foundUser: foundUserSlice,
-    filters: filtersSlice,
-    allShareHolder: allShareHolderSlice,
-    walletBalance: walletBalanceSlice
-  }
+const rootReducer = combineReducers({
+  user: userSlice,
+  allUsers: allUsersSlice,
+  foundUser: foundUserSlice,
+  filters: filtersSlice,
+  allShareHolder: allShareHolderSlice,
+  walletBalance: walletBalanceSlice
 });
-export const persistor = store;
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer
+});
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
